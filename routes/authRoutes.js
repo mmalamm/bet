6998f0dd256789a5d738d2d3836a162ca0f6a36f
Muhammad Alam <alam@mmtutor.com>
2018-12-06 22:28:44 -0500

    remove unnecessary routes from auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,55 +3,12 @@ const newUserHandler = require("./newUserHandler");
 const log = require("../config/log")("authRoutes!~");
 
 module.exports = app => {
-  ///
-  app.get("/login", (req, res) =>
-    res.send(`
-      <form action="/login" method="post">
-        <h1>Login</h1>
-        <div>
-          <label>Username:</label> <input type="text" name="username" /><br />
-        </div>
-        <div>
-          <label>Password:</label> <input type="password" name="password" />
-        </div>
-        <div><input type="submit" value="Submit" /></div>
-      </form>
-    `)
-  );
-  app.get("/register", (req, res) =>
-    res.send(`
-      <form action="/register" method="post">
-        <h1>Register</h1>
-        <div>
-          <label>Username:</label> <input type="text" name="username" /><br />
-        </div>
-        <div>
-          <label>Password:</label> <input type="password" name="password" />
-        </div>
-        <div><input type="submit" value="Submit" /></div>
-      </form>
-    `)
-  );
-  ///
+  app.post("auth/register", newUserHandler);
 
-  app.post("/register", newUserHandler);
-
-  app.post(
-    "/login",
-    passport.authenticate("local", { failureRedirect: "/failed" }),
-    (req, res) => {
-      // log(req.body);
-      // res.send(req.user);
-      res.cookie("username", req.user.username);
-      res.send(req.user);
-    }
-  );
   app.post(
     "/auth/local",
     passport.authenticate("local", { failureRedirect: "/failed" }),
     (req, res) => {
-      // log(req.body);
-      // res.send(req.user);
       res.cookie("username", req.user.username);
       res.send(req.user);
     }
