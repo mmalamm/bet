51b04072639cbd0269887954b7d05aa7e912ae2b
Muhammad Alam <alam@mmtutor.com>
2019-01-20 18:43:27 -0500

    start changepic form

--- frontend/src/components/ChangePicForm/ChangePicForm.js
+++ frontend/src/components/ChangePicForm/ChangePicForm.js
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import { updateIcon } from "../../actions/authActions";
 import { hideModal } from "../../actions/modalActions";
 
+import icons from "../../assets/icons";
+
 export class ChangePicForm extends Component {
   updateIcon = iconName => e => {
     this.props.updateIcon(iconName);
@@ -15,9 +17,16 @@ export class ChangePicForm extends Component {
     const { closeModal } = this.props;
     return (
       <div className={s.ChangePicForm}>
-        <button onClick={this.updateIcon("apple")}>apple</button>
-        <button onClick={this.updateIcon("bananas")}>bananas</button>
-        <button onClick={closeModal}>cherry</button>
+        <button onClick={closeModal}>Close</button>
+        <div className={s.buttonPanel}>
+          {Object.keys(icons).map(key => {
+            return (
+              <button key={key} onClick={this.updateIcon(key)} className={s.iconButton}>
+                <img className={s.buttonPic} src={icons[key]} alt={key} />
+              </button>
+            );
+          })}
+        </div>
       </div>
     );
   }
