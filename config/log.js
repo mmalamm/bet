dacd71234e898280ca218d988215ce3cc6a088c3
Muhammad Alam <alam@mmtutor.com>
2019-01-29 10:44:03 -0500

    add production URL

--- config/log.js
+++ config/log.js
@@ -1,9 +1,16 @@
 const chalk = require("chalk");
 
-module.exports = (w, clr = "blue") => (...args) => {
-  const now = Date.now();
-  console.log(chalk[clr](`[${new Date(now)}] ${w}:`));
-  console.log(...args);
-  const lineBreaker = `<-----------${now}----------->`;
-  console.log(`${chalk[clr](lineBreaker)}`);
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = (w, clr = "blue") => (...args) => {
+    const str = chalk[clr](`${Date.now()} ${w}`);
+    console.log(str, ...args);
+  };
+} else {
+  module.exports = (w, clr = "blue") => (...args) => {
+    const now = Date.now();
+    console.log(chalk[clr](`[${new Date(now)}] ${w}:`));
+    console.log(...args);
+    const lineBreaker = `<-----------${now}----------->`;
+    console.log(`${chalk[clr](lineBreaker)}`);
+  };
+}
