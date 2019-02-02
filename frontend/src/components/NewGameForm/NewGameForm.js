c3a806494cf65823605319a506cd6f751b85c122
Muhammad Alam <alam@mmtutor.com>
2019-02-02 03:38:10 -0500

    start new game form frontend

--- frontend/src/components/NewGameForm/NewGameForm.js
+++ frontend/src/components/NewGameForm/NewGameForm.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import s from './NewGameForm.module.scss';
+import s from "./NewGameForm.module.scss";
 
 export default class NewGameForm extends Component {
+  state = {
+    gameName: "",
+    inviteOnly: false,
+    invitedUsers: []
+  };
+
+  handleNameChange = e => {
+    const gameName = e.target.value;
+    this.setState({ gameName });
+  };
+
+  handlePrivacyToggle = e => {
+    const inviteOnly = e.target.checked;
+    this.setState({ inviteOnly });
+  };
+
+  handleSearchInput = e => {
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <div className={s.NewGameForm}>
-        new game form
+        <input type="text" onChange={this.handleNameChange} />
+        <input
+          type="checkbox"
+          name="inviteOnly"
+          onChange={this.handlePrivacyToggle}
+        />
+        <input type="text" onChange={this.handleSearchInput} />
       </div>
-    )
+    );
   }
 }
