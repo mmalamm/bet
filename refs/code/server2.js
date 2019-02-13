fb246b6d397053a502085fbcac7026d8eff9ca6b
Muhammad Alam <alam@mmtutor.com>
2019-02-12 23:54:12 -0500

    get clusters working

---
+++ refs/code/server2.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const path = require("path");
+
+const helmet = require("helmet");
+const passport = require("passport");
+
+const keys = require("../../config/keys");
+
+const log = require("../../config/log")("SERVER", "blue");
+
+const session = require("../../session");
+
+const authenticate = require("../../middlewares/authenticate");
+
+const app = express();
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser(keys.cookieKey));
+
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api", authenticate);
+
+require("../../models/User");
+require("../../services/passport");
+
+require("../../routes/authRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+const server = require("http").Server(app);
+require("../../services/socketio")(server);
+
+const PORT = process.env.PORT || 5050;
+
+server.listen(PORT, () => {
+  log("server running on port", PORT);
+});
+
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const path = require("path");
+const cluster = require("cluster");
+const net = require("net");
+
+const farmhash = require("farmhash");
 
 const helmet = require("helmet");
 const passport = require("passport");
@@ -14,36 +18,72 @@ const session = require("./session");
 
 const authenticate = require("./middlewares/authenticate");
 
-const app = express();
-app.use(helmet());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser(keys.cookieKey));
-
-app.use(session);
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/api", authenticate);
-
-require("./models/User");
-require("./services/passport");
-
-require("./routes/authRoutes")(app);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
-const server = require("http").Server(app);
-require("./services/socketio")(server);
-
 const PORT = process.env.PORT || 5050;
 
-server.listen(PORT, () => {
-  log("server running on port", PORT);
-});
+const num_processes = require("os").cpus().length;
+
+if (cluster.isMaster) {
+  log("master cluster is at work!");
+  const workers = [];
+
+  const spawn = i => {
+    workers[i] = cluster.fork();
+
+    workers[i].on("exit", (code, signal) => {
+      console.log("respawning worker", i);
+      spawn(i);
+    });
+  };
+
+  for (let i = 0; i < num_processes; i++) {
+    spawn(i);
+  }
+
+  const worker_index = (ip, len) => {
+    return farmhash.fingerprint32(ip) % len;
+  };
+
+  const server = net
+    .createServer({ pauseOnConnect: true }, socket => {
+      const worker = workers[worker_index(socket.remoteAddress, num_processes)];
+      worker.send("sticky-session:connection", socket);
+    })
+    .listen(PORT);
+} else {
+  log("child process is at work!");
+  const app = express();
+  app.use(helmet());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cookieParser(keys.cookieKey));
+
+  app.use(session);
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use("/api", authenticate);
+
+  require("./models/User");
+  require("./services/passport");
+
+  require("./routes/authRoutes")(app);
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+  }
+
+  const server = app.listen(0, "localhost");
+  require("./services/socketio")(server);
+
+  process.on("message", (message, connection) => {
+    if (message !== "sticky-session:connection") return;
+
+    server.emit("connection", connection);
+
+    connection.resume();
+  });
+}
